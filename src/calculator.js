5b17902116ce52f6003f5981bac47db3bfc63333
Joseph Daniel Chapdelaine <orvist@gmail.com>
2016-06-25 18:41:13 -0500

    all tests pass

---
+++ src/calculator.js
@@ -0,0 +1,25 @@
+"use strict";
+
+function add(x, y) {
+	return x + y;
+}
+
+function subtract(x, y) {
+	return x - y;
+}
+
+function multiply(x, y) {
+	return x * y;
+}
+
+function divide(x, y) {
+	return x / y;
+}
+
+function square(x) {
+	return Math.pow(x, 2);
+}
+
+function squareRoot(x) {
+	return Math.sqrt(x);
+}

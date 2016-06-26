dd07d3751803188da678c61a403775cc8a54675d
Joseph Daniel Chapdelaine <orvist@gmail.com>
2016-06-25 21:21:50 -0500

    calculator works

--- src/calculator.js
+++ src/calculator.js
@@ -1,7 +1,11 @@
 "use strict";
+var first;
+var second;
+var operation;
+
 
 function add(x, y) {
-	return x + y;
+ return x + y;
 }
 
 function subtract(x, y) {
@@ -23,3 +27,38 @@ function square(x) {
 function squareRoot(x) {
 	return Math.sqrt(x);
 }
+
+$(".button").click(function() {
+	first = $("#input").val();
+	$("#input").val("");
+	operation = this.id;
+});
+
+$("#input").keypress(function(e){
+	if(e.which == 13) {
+		second = $("#input").val();
+		runOp();
+	}
+});
+
+
+var runOp = function() {
+	if(operation === "add") {
+		$("#input").val(add(parseInt(first), parseInt(second)));
+	}
+	else if(operation === "sub") {
+		$("#input").val(subtract(parseInt(first), parseInt(second)));
+	}
+	else if(operation === "mul") {
+		$("#input").val(multiply(parseInt(first), parseInt(second)));
+	}
+	else if(operation === "div") {
+		$("#input").val(divide(parseInt(first), parseInt(second)));
+	}
+	else if(operation === "sqr") {
+		$("#input").val(square(parseInt(second)));
+	}
+	else if(operation === "sqrt") {
+		$("#input").val(squareRoot(parseInt(second)));
+	}
+};
